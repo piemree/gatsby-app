ebd980793701d128c4ad1abb73e3cbf24fae63ae
Emre <pi.emree@gmail.com>
2022-02-08 00:18:40 +0300

    Add custom responsive navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,23 +1,44 @@
 import { Link } from "gatsby"
-import React from "react"
-import { slide as Menu } from "react-burger-menu"
+import React, { useRef, useState } from "react"
 import "./Layout.css"
+import { GiHamburgerMenu } from "@react-icons/all-files/gi/GiHamburgerMenu";
 const Layout = ({ children }) => {
+  const [toggle, setToggle] = useState(false)
+  const mobileNavRef = useRef()
+
+  document.addEventListener("click", e => {
+    if (!e.composedPath().includes(mobileNavRef.current)) setToggle(false)
+  })
+
+  function handleClick(event) {
+    event.stopPropagation()
+    setToggle(true)
+  }
+
   return (
     <div>
-      <nav className="p-3 flex justify-between">
-        <h3 className="text-red-500">Logo</h3>
-        <ul className="hidden">
+      <nav className="p-3 flex justify-between shadow">
+        <h3 className="font-semibold text-xl ">U</h3>
+        <ul className="hidden md:flex">
           <li className="m-2">Home</li>
           <li className="m-2">Store</li>
           <li className="m-2">About</li>
         </ul>
-        <Menu right with={280}>
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-      
-        </Menu>
+        <button className="md:hidden" onClick={handleClick}>
+          <GiHamburgerMenu/>
+        </button>
+        <div
+          ref={mobileNavRef}
+          className={` z-10 bg-blue-100 absolute top-0 transition duration-200 ease-in-out  right-0 w-3/4 h-full ${
+            toggle ? "" : "translate-x-full"
+          } `}
+        >
+          <ul className="py-5 px-3">
+            <li className=" p-2 my-2 text-white font-semibold rounded cursor-pointer hover:bg-sky-500  bg-blue-400"><Link>Home</Link></li>
+            <li className=" p-2 my-2 text-white font-semibold rounded cursor-pointer hover:bg-sky-500  bg-blue-400"><Link>Store</Link></li>
+            <li className=" p-2 my-2 text-white font-semibold rounded cursor-pointer hover:bg-sky-500  bg-blue-400"><Link>About</Link></li>
+          </ul>
+        </div>
       </nav>
       {children}
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/Layout"
 export default function Home() {
   return (
     <Layout>
-      <div className="bg-gray-200">Hello world!</div>
+      <div className="">Hello world!</div>
     </Layout>
   )
 }
